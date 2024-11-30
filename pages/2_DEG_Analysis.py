fb178e6eaaf999188061aa51706b72de0d7fd813
Jheno Syechlo <jhenodahlia@gmail.com>
2024-11-30 08:51:46 +0700

    DEG

--- pages/2_DEG_Analysis.py
+++ pages/2_DEG_Analysis.py
@@ -4,6 +4,9 @@ from pydeseq2.dds import DeseqDataSet
 from pydeseq2.ds import DeseqStats
 import numpy as np
 
+# Advanced Styling
+st.set_page_config(page_title="Gene Expression Analysis", layout="wide")
+
 # App Title
 st.title("Differential Gene Expression Analysis with PyDESeq2")
 
@@ -58,17 +61,11 @@ if racial_dataset:
     cutoff_padj = st.number_input("Cutoff for padj", value=0.05)
     cutoff_log2FoldChange = st.number_input("Cutoff for log2FoldChange", value=0.0)
     cutoff_baseMean = st.number_input("Cutoff for baseMean", value=10)
-    cutoff_pvalue = st.number_input("Cutoff for pvalue", value=0.0)
-    cutoff_lfcSE = st.number_input("Cutoff for lfcSE", value=0.0)
-    cutoff_stat = st.number_input("Cutoff for stat", value=0.0)
 
     def filter_deg_results(deg_results):
         deg_results = deg_results[deg_results['padj'] < cutoff_padj]
         deg_results = deg_results[deg_results['log2FoldChange'].abs() > cutoff_log2FoldChange]
-        deg_results = deg_results[deg_results['baseMean'] > cutoff_baseMean]
-        deg_results = deg_results[deg_results['pvalue'] < cutoff_pvalue]
-        deg_results = deg_results[deg_results['lfcSE'] > cutoff_lfcSE]
-        deg_results = deg_results[deg_results['stat'].abs() > cutoff_stat]
+        deg_results = deg_results[deg_results['baseMean'] >= cutoff_baseMean]
         return deg_results
 
     filtered_deg_results = filter_deg_results(deg_stats_results)
